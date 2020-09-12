0150026c860716abd3a0ca528474eb46faedda60
LucaPalumbo <luca183.palumbo@gmail.com>
2020-09-12 15:55:51 +0200

    commit

--- code/analysis.C
+++ code/analysis.C
@@ -10,37 +10,19 @@ struct CapacitorData // structure containing data to be plotted
     Double_t avg_voltages[N_DATA]; // Y axis
     Double_t avg_times[N_DATA]; // X axis
     int experiment_counter = 0;
-
 };
 
-void readChargingData(std::ifstream &f,CapacitorData &c){
+void readData(std::ifstream &f,CapacitorData &c){
     Double_t time, voltage;
     std::string line;
     for(int i=0;i<N_DATA; i++){
         std::getline(f, line);
         std::istringstream iss(line);
         iss >> time >> voltage;
-        
         c.avg_times[i] = ((c.avg_times[i]*c.experiment_counter) + time/1000)/(c.experiment_counter+1);
         c.avg_voltages[i] = ((c.avg_voltages[i]*c.experiment_counter)+ voltage)/(c.experiment_counter+1);
-        
     }
     c.experiment_counter++;
-}
-void readDischargingData(std::ifstream &f,CapacitorData &d){
-    Double_t time, voltage;
-    std::string line;
-    for(int i=0;i<N_DATA;i++){
-        std::getline(f, line);
-        std::istringstream iss(line);
-        iss >> time >> voltage;
-        
-        d.avg_times[i] = ((d.avg_times[i]*d.experiment_counter) + time/1000)/(d.experiment_counter+1);
-        d.avg_voltages[i] = ((d.avg_voltages[i]*d.experiment_counter)+ voltage)/(d.experiment_counter+1);
-        
-    }
-    d.experiment_counter++;
-
 }
 
 void readFile(string fileName, CapacitorData &c, CapacitorData &d){    
@@ -53,17 +35,17 @@ void readFile(string fileName, CapacitorData &c, CapacitorData &d){
     }
     for(int i=0;i < 2; i++){
         std::getline(dataFile, line); // read "Experiment n. #"
-        readChargingData(dataFile, c);
+        readData(dataFile, c);
         std::getline(dataFile, line); // read "Discharging..."
-        readDischargingData(dataFile, d);
-    }
-    
+        readData(dataFile, d);
+    }   
 }
+
 void analysis() {
     CapacitorData charging;
     CapacitorData discharging;
     readFile("capacitor_data.txt", charging, discharging); // read data and fill charging/discharging structures
-    TCanvas *c = new TCanvas("c", "Capacitor data over time", 200, 200, 950,700); // create canvas
+    TCanvas *c = new TCanvas("c", "Capacitor D.D.P. over time", 200, 200, 950,700); // create canvas
     TGraph *graphFromData = new TGraph(N_DATA,charging.avg_times,charging.avg_voltages);
     graphFromData->Draw();  // draw graph of charging
     TF1 *expectedGraph = new TF1("expectedGraph","5*(1-e^(-x/1))",0,6);
@@ -72,4 +54,6 @@ void analysis() {
     legend->AddEntry(graphFromData,"d.d.p over time","l");
     legend->AddEntry(expectedGraph,"Expected result","l");
     legend->Draw();
+    graphFromData->GetYaxis()->SetTitle("D.D.P. [V]"); // set axis name
+    graphFromData->GetXaxis()->SetTitle("Time [s]"); // set axis name
 }
